1466f02ccfffa7a006f2dffcaf44900ec4d099ca
azkazunami36 <kazunami36.sum@icloud.com>
2022-10-30 16:03:28 +0900

    様々な機能の追加＆変更 応答機能の追加 サーバー参加時のようこそメッセージ追加 etc...

--- main.js
+++ main.js
@@ -1,39 +1,27 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Partials, SlashCommandBuilder, EmbedBuilder, ApplicationCommandType, ApplicationCommandOptionType, SnowflakeUtil } = require('discord.js'), //これ使って一部の機能が使える。
+const { Client, GatewayIntentBits, Partials, EmbedBuilder, BaseChannel, ApplicationCommandType, ApplicationCommandOptionType, ChannelType, SlashCommandBuilder, PresenceUpdateStatus, DMChannel } = require("discord.js"),
+{ entersState, createAudioPlayer, createAudioResource, joinVoiceChannel, StreamType, AudioPlayerStatus } = require("@discordjs/voice"),
 	client = new Client({
-		partials: [Partials.Channel],
-		intents: [
-			GatewayIntentBits.Guilds,
-			GatewayIntentBits.GuildVoiceStates,
-			GatewayIntentBits.GuildMessages,
-			GatewayIntentBits.GuildMessageReactions,
-			GatewayIntentBits.DirectMessages,
-			GatewayIntentBits.MessageContent
-		]
+		partials: [Partials.Channel, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.Message, Partials.Reaction, Partials.ThreadMember, Partials.User],
+		intents: [GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent]
 	}),
+	ytdl = require('ytdl-core'), //YouTube Downloadのコア
 	data = require("./data.json");
-let channellog,
-	br = "\n",
-	bro = "```",
-	scommand = [
-		new SlashCommandBuilder()
-			.setName("help")
-			.setDescription("ヘルプを表示します。")
-	],
-	texts = data.texts;
-for (let i = 0; i != data.data.length; i++) {
-	scommand.push(new SlashCommandBuilder()
-		.setName(data.data[i].command)
-		.setDescription(data.data[i].name));
-};
+let scommand = [
+	new SlashCommandBuilder()
+		.setName("help")
+		.setDescription("ヘルプを表示します。")
+];
+
+for (let i = 0; i != data.data.length; i++) { scommand.push(new SlashCommandBuilder().setName(data.data[i].command).setDescription(data.data[i].name)); };
+
 client.on('ready', () => {
 	try {
-		channellog = client.channels.cache.get(data.channel_log);
 		client.user.setPresence({ activities: [{ name: data.playing }], status: "online" }); //ステータス設定
 		client.application.commands.set(scommand); //botにセット
 		console.log("Bot準備完了～\nnode.js:" + process.version + "\nDiscord.js:v" + require('discord.js').version); //ログインできたか確認＆バージョン参照
 	} catch (e) {
-		errorsend("ready", e);
+		console.log(e);
 	};
 });
 client.on('interactionCreate', interaction => {
@@ -48,7 +36,7 @@ client.on('interactionCreate', interaction => {
 							.setTitle("ヘルプ")
 							.setDescription("今調べられる都道府県の数は、`" + data.data.length + "`個調べられます")
 							.setColor(0x7289da)
-							.addFields({ name: "botについて", value: autobr(texts[0]) })
+							.addFields({ name: "botについて", value: autobr(data.texts[0]) })
 					]
 				}); //送信
 				break;
@@ -66,16 +54,17 @@ client.on('interactionCreate', interaction => {
 							]
 						});
 						set = true; //当たったことにする
+						break;
 					};
 				};
 				if (set != true) { //当たらなかったらif実行。でも他のコマンドはないから、下の文字列を返す
-					interaction.reply({ content: autobr(texts[1]), ephemeral: true });
+					interaction.reply({ content: autobr(data.texts[1]), ephemeral: true });
 				};
 				break;
 			}
 		};
 	} catch (e) {
-		errorsend("interactionCreate", e);
+		console.log(e);
 	};
 });
 client.on('messageCreate', message => {
@@ -84,36 +73,43 @@ client.on('messageCreate', message => {
 		if (message.mentions.users.has(client.user.id) || message.mentions.roles.some(r => [client.user.username].includes(r.name)) ? true : false) {
 			message.reply("呼びましたか？コマンド使ってくれないと怒りますっ！");
 		};
+		for (let i = 0; data.reply.length != i; i++) {
+			for (let Ii = 0; data.reply[i].message.length != Ii; Ii++) {
+				if (message.content.match(data.reply[i].message[Ii])) {
+					message.channel.send(data.reply[i].reply[Math.floor(Math.random() * data.reply[i].reply.length)]);
+				};
+			};
+		};
 	} catch (e) {
-		errorsend("messageCreate", e);
+		console.log(e);
+	};
+});
+client.on("guildMemberAdd", async member => {
+	try {
+		for (let i = 0; i != (Object.keys(data.replychannels.channel).length); i++) {
+			let id = Object.keys(data.replychannels.channel)[i];
+			if (member.guild.id == id) {
+				client.channels.fetch(data.replychannels.channel[id].welcome).then(channel => {
+					let message = "**" + member.user.username + "**さんようこそ" + member.guild.name + "へ！\nあなたは" + member.guild.memberCount + "人目のメンバーです！";
+					channel.send(message);
+				});
+			};
+		};
+	} catch (e) {
+		console.log(e);
 	};
 });
 function autobr(textdata) {
-	var outdata = "";
-	for (var i = 0; i != textdata.length; i++) {
-		if (outdata != "") {
-			outdata = outdata + br;
-		};
-		outdata = outdata + textdata[i];
+	let outdata = "";
+	for (let i = 0; i != textdata.length; i++) {
+		if (outdata != "") outdata += "\n";
+		outdata += textdata[i];
 	};
 	return outdata;
 };
-function errorsend(set, e) {
-	console.log(e); console.log(set);
-	if (0 == 1) channellog.send({
-		embeds: [{
-			title: "エラー検知",
-			description: set + autobr(texts[2]),
-			fields: [{
-				name: "内容",
-				value: bro + e + bro
-			}],
-			timestamp: new Date()
-		}]
-	});
-};
-if (process.env.IBUKI_BOT_TOKEN == undefined || process.env.IBUKI_BOT_TOKEN == "") {
+if (process.env.IBUKI_BOT_TOKEN) {
+	client.login(process.env.IBUKI_BOT_TOKEN);
+} else {
 	console.log('トークンがundefinedとなっていたため、実行を停止します。');
 	process.exit(0);
 };
-client.login(process.env.IBUKI_BOT_TOKEN);
